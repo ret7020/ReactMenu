dfd6ff7c7199ed149482ffce07c5ce89421eca2c
ret7020 <ret7020@gmail.com>
2023-04-25 13:29:20 +0300

    Lazy loading base

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} fallbackElement={<>Loading...</>}/>
   </React.StrictMode>
 );
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,36 +4,44 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 
+
+
 // Pages
-
-import { Notifications } from "../Pages/Notifications.jsx";
-import { Events } from "../Pages/Events.jsx";
-import { Docs } from "../Pages/Docs.jsx";
-import { Me } from "../Pages/Me.jsx";
-
 export const routes = [
   {
     index: true,
-    path: '',
-    element: <Me />,
+    path: '', // Hack
+    async lazy() {
+      let { Me } = await import("../Pages/Me.jsx");
+      return { Component: Me };
+    },
     icon: <UserOutlined />,
     label: 'Аккаунт'
   },
   {
     path: 'events',
-    element: <Events/>,
+    async lazy() {
+      let { Events } = await import("../Pages/Events.jsx");
+      return { Component: Events };
+    },
     icon: <UserOutlined />,
     label: 'Мероприятия'
   },
   {
     path: 'notifications',
-    element: <Notifications/>,
+    async lazy() {
+      let { Notifications } = await import("../Pages/Notifications.jsx");
+      return { Component: Notifications };
+    },
     icon: <UserOutlined />,
     label: 'Уведомления'
   },
   {
     path: 'docs',
-    element: <Docs/>,
+    async lazy() {
+      let { Docs } = await import("../Pages/Docs.jsx");
+      return { Component: Docs };
+    },
     icon: <UserOutlined />,
     label: 'Документация'
   }
